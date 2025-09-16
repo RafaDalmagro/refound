9f43ec4d5a0cf5158e6d876e2028f84e95ad0d04
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-16 11:29:48 -0300

    Criando metodo para envio dos dados dos inputs.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 export function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    function onSubmit(event: React.FormEvent) {
+        event.preventDefault();
+
+        console.log(email, password);
+    }
+
     return (
-        <form className="w-full flex flex-col gap-4">
+        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
             <Input
                 required
                 legend="Email"
                 type="email"
                 placeholder="seu@email.com"
+                onChange={(event) => setEmail(event.target.value)}
             />
             <Input
                 required
                 legend="Senha"
                 type="password"
                 placeholder="123456"
+                onChange={(event) => setPassword(event.target.value)}
             />
-            <Button isLoading>Entrar</Button>
+            <Button type="submit" isLoading={isLoading}>
+                Entrar
+            </Button>
+            <a
+                href="/signup"
+                className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">
+                Criar conta
+            </a>
         </form>
     );
 }
