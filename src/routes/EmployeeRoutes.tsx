3984b22375a11e6f4781d83b53742ab40f286fa3
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-16 13:50:54 -0300

    Criando layout geral da pagina.

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -2,11 +2,14 @@ import { Route, Routes } from "react-router";
 
 import { Refound } from "../pages/Refound";
 import { NotFound } from "../pages/NotFound";
+import { AppLayout } from "../components/AppLayout";
 
 export function EmployeeRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Refound />} />
+            <Route path="/" element={<AppLayout />}>
+                <Route index element={<Refound />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
     );
