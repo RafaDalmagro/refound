9a0a71e6af202878b37a5de81e899e7d5202bd61
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-17 19:23:55 -0300

    Estilizando o RefundItem.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,19 @@
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
-import { useState } from "react";
+import { RefundItem } from "../components/RefundItem";
 import searchSvg from "../assets/search.svg";
+import { CATEGORIES } from "../utils/categories";
+
+import { useState } from "react";
+
+const REFUND_EXAMPLE = {
+    id: "1",
+    username: "João Pedro",
+    category: "Alimentação",
+    amount: "25,00",
+    categoryImg: CATEGORIES["transport"].icon,
+};
+
 export function Dashboard() {
     const [name, setName] = useState("");
 
@@ -31,6 +43,9 @@ export function Dashboard() {
                     />
                 </Button>
             </form>
+            <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
+                <RefundItem data={REFUND_EXAMPLE} />
+            </div>
         </div>
     );
 }
