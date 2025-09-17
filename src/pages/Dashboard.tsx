d187d27c1dbe8170a921d06a5528f9e8b514f353
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-17 13:44:23 -0300

    Estilizando input inicial do formulario.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,28 @@
+import { Input } from "../components/Input";
+import { useState } from "react";
+
 export function Dashboard() {
-    return <h1>Painel de Controle</h1>;
+    const [name, setName] = useState("");
+
+    function fetchRefounds(e: React.FormEvent) {
+        e.preventDefault();
+
+        console.log(name);
+    }
+
+    return (
+        <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px] ">
+            <h1 className="text-gray-100 font-bold text-xl flex-1">
+                Solicitações
+            </h1>
+            <form
+                onSubmit={fetchRefounds}
+                className="flex items-center justify-center pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-3 mt-6">
+                <Input
+                    placeholder="Pesquisar pelo nome"
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </form>
+        </div>
+    );
 }
