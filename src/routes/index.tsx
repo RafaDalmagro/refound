dec8990683bb485b0ac7241356fac7016605b48f
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-20 09:17:26 -0300

    Implementando componente de Loading.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,20 @@ import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
+import { Loading } from "../components/Loading";
+
+const isLoading = true;
 
 import { ManagerRoutes } from "./ManagerRoutes";
 export function Routes() {
+    if (isLoading) {
+        return <Loading />;
+    }
     return (
         <BrowserRouter>
             {/* <AuthRoutes /> */}
             {/* <EmployeeRoutes /> */}
-            <ManagerRoutes />
+            <AuthRoutes />
         </BrowserRouter>
     );
 }
