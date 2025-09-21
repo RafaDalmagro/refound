51c184d0caae55074e24de4e8fcaef8385b629a6
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-21 16:29:04 -0300

    Permanecendo os dados do usuario com localstorage e incluindo funcão de deslogar.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,22 +1,56 @@
-import { createContext } from "react";
-import { useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 type AuthContext = {
+    isLoading: boolean;
     session: null | UserAPIResponse;
     save: (session: UserAPIResponse) => void;
+	remove: () => void;
 };
+
+const LOCAL_STORAGE_KEY = "@refound";
 
 export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
     const [session, setSession] = useState<null | UserAPIResponse>(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-    function save(session: UserAPIResponse) {
-        setSession(session);
+    function save(data: UserAPIResponse) {
+        localStorage.setItem(
+            `${LOCAL_STORAGE_KEY}:user`,
+            JSON.stringify(data.user)
+        );
+        localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
+
+        setSession(data);
     }
 
+    function loadUser() {
+        const user = localStorage.getItem(`${LOCAL_STORAGE_KEY}:user`);
+        const token = localStorage.getItem(`${LOCAL_STORAGE_KEY}:token`);
+
+        if (user && token) {
+            setSession({ user: JSON.parse(user), token });
+        }
+
+        setIsLoading(false);
+    }
+
+    function remove() {
+        setSession(null);
+        localStorage.removeItem(`${LOCAL_STORAGE_KEY}:user`);
+        localStorage.removeItem(`${LOCAL_STORAGE_KEY}:token`);
+
+		window.location.href = "/";
+    }
+
+    useEffect(() => {
+        loadUser();
+    }, []);
+
     return (
-        <AuthContext.Provider value={{ session, save }}>
+        <AuthContext.Provider value={{ session, save, isLoading, remove }}>
             {children}
         </AuthContext.Provider>
     );
